047a4660605b04ee597c4465d971c62347d29429
murphylee2048 <1134697131@qq.com>
2019-07-15 08:43:28 +0800

    test : should_always_park_car_to_larger_available_position_rate_parkinglot

---
+++ src/test/java/com/thoughtworks/tdd/SuperSmartBoyTest.java
@@ -0,0 +1,28 @@
+package com.thoughtworks.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SuperSmartBoyTest {
+    // story 5
+    @Test
+    public void should_always_park_car_to_larger_available_position_rate_parkinglot() throws Exception {
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot(10);
+        ParkingLot parkingLot2 = new ParkingLot(5);
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy();
+
+        // when
+        for (int i = 0; i <= 7; i++) {
+            superSmartParkingBoy.park(car);
+        }
+
+        // then
+        assertEquals(parkingLot1.getSize(), parkingLot1.getSize());
+        assertEquals(parkingLot2.getSize(), parkingLot2.getSize());
+
+
+    }
+}
